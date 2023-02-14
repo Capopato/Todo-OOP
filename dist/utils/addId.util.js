76bae7857c13ed19a07bb28daed2df68a20ce762
Capopato <107575282+Capopato@users.noreply.github.com>
2023-02-14 13:46:26 +0100

    Delete dist directory

--- dist/utils/addId.util.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addId = void 0;
-const uuid_1 = require("uuid");
-const addId = () => {
-    return (0, uuid_1.v4)();
-};
-exports.addId = addId;
--- dist/utils/throwError.utils.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// export function throwError(error: unknown): asserts error is Error {
-//   if (!(error instanceof Error)) {
-//     throw error;
-//   }
-// }
