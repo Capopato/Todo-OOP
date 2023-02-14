9307907b71355c2fc81fa84f1a21b7bd8490a198
Capopato <107575282+Capopato@users.noreply.github.com>
2023-02-14 13:46:19 +0100

    Delete src directory

--- src/models/user.model.ts
+++
@@ -1,7 +0,0 @@
-import { TodoList } from "../classes/todolist.class";
-
-export interface userModel {
-  id: string;
-  name: string;
-  addList(list: TodoList): void;
-}
--- src/utils/error.util.ts
+++
@@ -1,6 +0,0 @@
-export function throwError(error: unknown): asserts error is Error {
-  if (!(error instanceof Error)) {
-    console.log(error);
-    throw error;
-  }
-}
